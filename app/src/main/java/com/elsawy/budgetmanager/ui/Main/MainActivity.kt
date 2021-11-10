169558602df4bf9493fca0e0b672afd63ed66c8c
Ahmed7Elsawy <ahmedelsawy487@gmail.com>
2021-11-10 07:01:02 +0200

    update custom dialog style

--- app/src/main/java/com/elsawy/budgetmanager/ui/Main/MainActivity.kt
+++ app/src/main/java/com/elsawy/budgetmanager/ui/Main/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.elsawy.budgetmanager.ui.Main
 
 import android.app.DatePickerDialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -20,101 +22,124 @@ import com.elsawy.budgetmanager.R
 import com.elsawy.budgetmanager.data.local.Action
 import com.elsawy.budgetmanager.data.local.Category
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.button.MaterialButton
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import dagger.hilt.android.AndroidEntryPoint
 import java.text.SimpleDateFormat
 import java.util.*
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private val mainActivityViewModel: MainActivityViewModel by viewModels()
+   private val mainActivityViewModel: MainActivityViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      setContentView(R.layout.activity_main)
 
-        val fab = findViewById<FloatingActionButton>(R.id.fab)
-        fab.setOnClickListener {
-            launchCustomDialog()
-        }
+      val fab = findViewById<FloatingActionButton>(R.id.fab)
+      fab.setOnClickListener {
+         launchCustomDialog()
+      }
+      setupBottomNavigation()
+   }
 
-        setupBottomNavigation()
+   private fun setupBottomNavigation() {
+      val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+      bottomNavigationView.background = null
+      bottomNavigationView.menu.get(1).isEnabled = false
+      val navController = findNavController(R.id.nav_fragment)
+      bottomNavigationView.setupWithNavController(navController)
+   }
 
-    }
+   private fun launchCustomDialog() {
+      val customLayout: View =
+         LayoutInflater.from(this@MainActivity).inflate(R.layout.custom_dialog, null, false)
+      val customDialog = AlertDialog.Builder(this, 0).create()
+      customDialog.apply {
+         window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+         setView(customLayout)
+      }.show()
 
-    private fun setupBottomNavigation() {
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        bottomNavigationView.background = null
-        bottomNavigationView.menu.get(1).isEnabled = false
-        val navController = findNavController(R.id.nav_fragment)
-        bottomNavigationView.setupWithNavController(navController)
-    }
 
+      val dateTextView = customLayout.findViewById<TextView>(R.id.date_textview)
+      val categoryTextview = customLayout.findViewById<TextView>(R.id.category_textview)
+      val descriptionEditText = customLayout.findViewById<TextView>(R.id.description_edittext)
+      val amountEdittext = customLayout.findViewById<TextView>(R.id.amount_edittext)
+      val saveButton = customLayout.findViewById<MaterialButton>(R.id.save_dialog_btn)
+      val cancelButton = customLayout.findViewById<MaterialButton>(R.id.cancel_dialog_btn)
 
-    private fun launchCustomDialog() {
-        val customLayout =
-            LayoutInflater.from(this@MainActivity).inflate(R.layout.custom_dialog, null)
+      var selectedDate: Date = Date()
+      mainActivityViewModel.dateLiveData.observe(this) { date ->
+         val simpleDateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.US)
+         dateTextView.text = simpleDateFormat.format(date)
+         selectedDate = date
+      }
 
-        val dateTextView = customLayout.findViewById<TextView>(R.id.date_textview)
-        dateTextView.text = SimpleDateFormat("dd.MM.yyyy").format(System.currentTimeMillis())
-        val categoryTextview = customLayout.findViewById<TextView>(R.id.category_textview)
-        val descriptionEditText = customLayout.findViewById<TextView>(R.id.description_edittext)
-        val amountEdittext = customLayout.findViewById<TextView>(R.id.amount_edittext)
+      dateTextView.setOnClickListener {
+         selectDate()
+      }
+      categoryTextview.setOnClickListener { v: View ->
+         showMenu(v, R.menu.category_menu, categoryTextview)
+      }
 
-        val calendar = Calendar.getInstance()
-        val dateSetListener =
-            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
-                calendar.set(Calendar.YEAR, year)
-                calendar.set(Calendar.MONTH, monthOfYear)
-                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-                val myFormat = "dd.MM.yyyy" // mention the format you need
-                val simpleDateFormat = SimpleDateFormat(myFormat, Locale.US)
-                dateTextView.text = simpleDateFormat.format(calendar.time)
-            }
+      saveButton.setOnClickListener {
+         val description = descriptionEditText.text.toString()
+         val amount = amountEdittext.text.toString()
+         val category = categoryTextview.text.toString()
 
-        dateTextView.setOnClickListener {
-            DatePickerDialog(this@MainActivity, dateSetListener,
-                calendar.get(Calendar.YEAR),
-                calendar.get(Calendar.MONTH),
-                calendar.get(Calendar.DAY_OF_MONTH)).show()
-        }
+         if (isCategoryValid(category) && isAmountValid(amount)) {
+            val action = Action(selectedDate.time,
+               Category.valueOf(category.uppercase()),
+               description,
+               amount.toDouble())
 
-        categoryTextview.setOnClickListener { v: View ->
-            showMenu(v, R.menu.category_menu,categoryTextview)
-        }
+            Log.d("Action", action.toString())
+            mainActivityViewModel.insertAction(action)
+            // Dismiss Dialog
+            customDialog.dismiss()
+         } else {
+            if (!isAmountValid(amount))
+               Toast.makeText(this@MainActivity, "put the Amount", Toast.LENGTH_SHORT).show()
+            if (!isCategoryValid(category))
+               Toast.makeText(this@MainActivity, "Select Category", Toast.LENGTH_SHORT).show()
+         }
+      }
 
-        val builder = AlertDialog.Builder(this@MainActivity)
-            .setView(customLayout)
-            .setPositiveButton("Save") { dialogInterface, _ ->
+      cancelButton.setOnClickListener {
+         customDialog.cancel()
+      }
 
-                val action = Action(calendar.timeInMillis,
-                    Category.valueOf(categoryTextview.text.toString().toUpperCase()),
-                    descriptionEditText.text.toString(),
-                    amountEdittext.text.toString().toDouble()
-                )
+   }
 
-                Log.d("Action",action.toString())
-                mainActivityViewModel.insertAction(action)
-                // Dismiss Dialog
-                dialogInterface.dismiss()
+   private fun selectDate() {
+      val calendar = Calendar.getInstance()
+      val dateSetListener =
+         DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, monthOfYear)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            mainActivityViewModel.updateDate(calendar.time)
+         }
+      DatePickerDialog(this@MainActivity, dateSetListener,
+         calendar.get(Calendar.YEAR),
+         calendar.get(Calendar.MONTH),
+         calendar.get(Calendar.DAY_OF_MONTH)).show()
+   }
 
-                Toast.makeText(this@MainActivity, "message", Toast.LENGTH_SHORT).show()
-            }
-            .setNegativeButton("Cancel") { dialogInterface, _ -> dialogInterface.cancel() }
-        builder.show()
-    }
+   private fun isCategoryValid(category: String) = category != getString(R.string.category)
 
-    private fun showMenu(v: View, @MenuRes menuRes: Int, categoryTextview: TextView) {
-        val popup = PopupMenu(this, v)
-        popup.menuInflater.inflate(menuRes, popup.menu)
+   private fun isAmountValid(amount: String) = amount.isNotEmpty()
 
-        popup.setOnMenuItemClickListener { menuItem: MenuItem ->
-            categoryTextview.text = menuItem.title
-            true
-        }
-        popup.show()
-    }
+   private fun showMenu(v: View, @MenuRes menuRes: Int, categoryTextview: TextView) {
+      val popup = PopupMenu(this, v)
+      popup.menuInflater.inflate(menuRes, popup.menu)
+
+      popup.setOnMenuItemClickListener { menuItem: MenuItem ->
+         categoryTextview.text = menuItem.title
+         true
+      }
+      popup.show()
+   }
 
 }
--- app/src/main/java/com/elsawy/budgetmanager/ui/Main/MainActivityViewModel.kt
+++ app/src/main/java/com/elsawy/budgetmanager/ui/Main/MainActivityViewModel.kt
@@ -1,6 +1,7 @@
 package com.elsawy.budgetmanager.ui.Main
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.elsawy.ahmed.news.data.provider.PreferencesHelper
@@ -11,27 +12,40 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
 class MainActivityViewModel @Inject constructor(
-    private val repository: ActionRepository,
-    private val sharedPreferences: PreferencesHelper,
+   private val repository: ActionRepository,
+   private val sharedPreferences: PreferencesHelper,
 ) : ViewModel() {
 
-    fun insertAction(action: Action) {
-        var balance = sharedPreferences.getBalance()
-        if (action.category == Category.INCOME)
-            balance += action.amount
-        else
-            balance -= action.amount
-        action.balance = balance
-        sharedPreferences.setBalance(balance)
-        viewModelScope.launch(Dispatchers.IO) {
-            repository.insertActionItem(action)
-        }
-        Log.d("ActionVM",action.toString())
-    }
+   private val _dateLiveData = MutableLiveData<Date>()
+   var dateLiveData = _dateLiveData
+
+   init {
+      _dateLiveData.value = Date(System.currentTimeMillis())
+   }
+
+   fun updateDate(date: Date) {
+      _dateLiveData.value = date
+   }
+
+   fun insertAction(action: Action) {
+      var balance = sharedPreferences.getBalance()
+      if (action.category == Category.INCOME)
+         balance += action.amount
+      else
+         balance -= action.amount
+      action.balance = balance
+      sharedPreferences.setBalance(balance)
+      viewModelScope.launch(Dispatchers.IO) {
+         repository.insertActionItem(action)
+      }
+      Log.d("ActionVM", action.toString())
+   }
 
 
 }
